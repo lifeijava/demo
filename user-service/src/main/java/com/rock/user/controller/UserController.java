f979db2b8a37e206ee10a97501283b4de6511c6b
lifei <lifeijava@163.com>
2020-02-24 15:31:34 +0800

    ip

--- user-service/src/main/java/com/rock/user/controller/UserController.java
+++ user-service/src/main/java/com/rock/user/controller/UserController.java
@@ -2,10 +2,11 @@ package com.rock.user.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/user")
@@ -26,5 +27,14 @@ public class UserController {
     public String getUser(@PathVariable("id") String id) {
         System.out.println("接收到请求[/user/" + id + "]");
         return "testUser:"+port;
+    }
+    @RequestMapping(value = "/time", method = RequestMethod.GET)
+    public String hello(HttpServletRequest request){
+
+        return "hello, "
+                + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())
+                + ", extendtag ["
+                + request.getHeader("extendtag")
+                + "]";
     }
 }
